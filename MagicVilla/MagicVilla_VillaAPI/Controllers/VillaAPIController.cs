5cd24ad139833dd3a05b6454e065c9441be79727
Maciej Przygoda <maciekprzygoda@interia.pl>
2023-08-15 18:40:25 +0200

    Implemented Dto models, VillaStore and GetVilla by Id method in Controller

--- MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,20 +1,25 @@
 ﻿using MagicVilla_VillaAPI.Model;
+using MagicVilla_VillaAPI.Model.Dto;
 using Microsoft.AspNetCore.Mvc;
+using MagicVilla_VillaAPI.Data;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
+    //[Route("api/[controller]")]
     [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Villa> GetVillas()
+        public IEnumerable<VillaDTO> GetVillas()
         {
-            return new List<Villa>()
-            {
-                new Villa{Id = 1, Name ="Pool View" },
-                new Villa{Id = 2, Name ="Beach View"}
-            };
+            return VillaStore.villalist;
+        }
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villalist.FirstOrDefault(u => u.Id == id);
         }
     }
 }
