b18d93681ebb0727748480dbb485079592aa46ce
Maciej Przygoda <maciekprzygoda@interia.pl>
2023-08-15 19:37:14 +0200

    Added required field for creating Villas and implemented check for unique Villa Name

--- MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -42,6 +42,15 @@ namespace MagicVilla_VillaAPI.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO)
         {
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+            if (VillaStore.villalist.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("CustomError", "Villa already Exists!");
+                return BadRequest(ModelState);
+            }
             if (villaDTO == null)
             {
                 return BadRequest(villaDTO);
