5a46b832cbbc0123471e47ea5111ef6005b83078
Maciej Przygoda <maciekprzygoda@interia.pl>
2023-10-02 14:43:33 +0200

    Implemented API type and mapping in Web project

---
+++ MagicVilla/MagicVilla_Web/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Model;
+using MagicVilla_Web.Model.Dto;
+
+namespace MagicVilla_VillaAPI
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+
+            CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+
+        }
+    }
+}
