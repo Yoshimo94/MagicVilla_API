fe9167c6ad2d8b1b2cc8c383fe9131c5454b5920
Maciej Przygoda <maciekprzygoda@interia.pl>
2023-12-29 13:48:04 +0100

    Fixed issue services

--- MagicVilla/MagicVilla_Web/Services/IServices/IVillaNumberService.cs
+++ MagicVilla/MagicVilla_Web/Services/IServices/IVillaNumberService.cs
@@ -6,8 +6,8 @@ namespace MagicVilla_Web.Services.IServices
     {
         Task<T> GetAllAsync<T>();
         Task<T> GetAsync<T>(int id);
-        Task<T> CreateAsync<T>(VillaCreateDTO dto);
-        Task<T> UpdateAsync<T>(VillaUpdateDTO dto);
+        Task<T> CreateAsync<T>(VillaNumberCreateDTO dto);
+        Task<T> UpdateAsync<T>(VillaNumberUpdateDTO dto);
         Task<T> DeleteAsync<T>(int id);
     }
 }
