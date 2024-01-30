ca1681e6c798c0d4cea7f9e5f08eb203a0675091
Maciej Przygoda <maciekprzygoda@interia.pl>
2024-01-30 16:33:40 +0100

    Implemented UserRepo with Register and IsUniqueUser methods

---
+++ MagicVilla/MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Model;
+using MagicVilla_VillaAPI.Model.Dto;
+using MagicVilla_VillaAPI.Repository.IRepository;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool IsUniqueUser(string username)
+        {
+            var user = _db.LocalUsers.FirstOrDefault(x => x.UserName == username);
+            if (user == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            LocalUser user = new LocalUser()
+            {
+                UserName = registerationRequestDTO.UserName,
+                Name = registerationRequestDTO.Name,
+                Password = registerationRequestDTO.Password,
+                Role = registerationRequestDTO.Role,
+            };
+
+            _db.LocalUsers.Add(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
+        }
+    }
+}
