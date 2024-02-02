d3daaef5ac40dfa9f3fb82b2285618d6692b384e
Maciej Przygoda <maciekprzygoda@interia.pl>
2024-02-02 14:44:02 +0100

    Added AuthController with methods

---
+++ MagicVilla/MagicVilla_Web/Controllers/AuthController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Model.Dto;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        private readonly IMapper _mapper;
+
+        public AuthController(IAuthService authService, IMapper mapper)
+        {
+            _authService = authService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDTO obj = new();
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginRequestDTO obj)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterationRequestDTO obj)
+        {
+            APIResponse result = await _authService.RegisterAsync<APIResponse>(obj);
+            if(result != null && result.IsSuccess)
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
