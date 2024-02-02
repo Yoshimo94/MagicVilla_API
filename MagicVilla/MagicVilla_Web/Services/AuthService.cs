6b05b7716ccfcefa27111c17afe7596a7dcce8c4
Maciej Przygoda <maciekprzygoda@interia.pl>
2024-02-02 14:29:53 +0100

    Added AuthService

---
+++ MagicVilla/MagicVilla_Web/Services/AuthService.cs
@@ -0,0 +1,38 @@
+﻿using MagicVilla_Utility;
+using MagicVilla_Web.Model.Dto;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Services.IServices;
+
+namespace MagicVilla_Web.Services
+{
+    public class AuthService : BaseService, IAuthService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private string villaUrl;
+        public AuthService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+            villaUrl = configuration.GetValue<string>("ServicesUrls:VillaAPI");
+        }
+
+        public Task<T> LoginAsync<T>(LoginRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Url = villaUrl + "/api/UsersAuth/login",
+                Data = obj
+            });
+        }
+
+        public Task<T> RegisterAsync<T>(RegisterationRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Url = villaUrl + "/api/UsersAuth/register",
+                Data = obj
+            });
+        }
+    }
+}
